7f917c690678a44fc07cea701fb85c8d3f971840
Stavon Ewart <stavon@Stavons-MacBook-Air.local>
2020-08-07 09:06:23 -0400

    Updates readme file and adds psuedocode

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dawn & Dusk
+## Unit 1 Project for General Assembly SEI
+
+I think sunrise and sunset are the most beatiful times of the day, so I chose to make an app that will let you find the times of sunrise/sunset for any given location. 
+
+![wireframe](https://i.imgur.com/BPOnFUw.png "Basic layout")
+
---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Add HTML for Header and body images
+// Add HTML for Darkmode button
+// Add input field for city/town name
+	// is there a zip code option?
+// add container element 'card's for the results to appear in
+	// sunset card
+	// sunrise card
+// add cached element references for the container element in JS file
+// add cached element reference for the input field
+// add cached element reference for the dark/light button
+// add cached element for go button
+// add event listener for input field
+// add event listener for go button
+// add event listener for dark/light mode button
+// write code for API call and console.log
+	// The town/city input will return a latitude/longitude
+	// use this information to call the 2nd API to return the results
+	// perhaps a function?
+// Tweak event listeners so that the results will appear in a variable
+// create a test 'card' element with Bootstrap
+// add a function to handle appending the sunset card
+// add a function to handle appending the sunrise card
+// style the cards with sunset/sunrise images respectively 
+	// may want to consider displaying the result text over the image
+// add a reset button? or something that deletes the returned results so you can enter a new place
