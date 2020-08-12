b1e57b87ed866677aa1cc3a42af9b7f77051bbd4
Stavon Ewart <stavon@Stavons-MacBook-Air.local>
2020-08-12 14:46:17 -0400

    Fixes mobile view bugs

--- js/main.js
+++ js/main.js
@@ -1,31 +1,3 @@
-// // Add body images
-// // Add HTML for Darkmode button
-// // add HTML for a reset button that removes the added cards
-// // Add HTML for an input field for zipcode
-// // add container element 'card's for the results to appear in
-	// // sunset card
-	// // sunrise card
-// // add cached element references for the container element in JS file
-// // add cached element reference for the dark/light button
-// // add cached element for go button
-// // add cached element for the reset button
-// // add event listener for input field
-// // add event listener for go button
-// // add event listener for dark/light mode button
-// write code for API call and console.log
-	// // The town/city input will return a latitude/longitude
-	//  //use this information to call the 2nd API to return the results
-    // Change time to user's location time
-// //Tweak event listeners so that the results will appear in a variable
-// // add a function to handle appending the sunset card
-// // add a function to handle appending the sunrise card
-// style the cards with sunset/sunrise images respectively 
-    // may want to consider displaying the result text over the image
-    
-// create a function that translates UTC time into the user's local time 
-
-
-
 const goBtn = document.getElementById('go')
 const container = document.getElementById('container')
 const riseTitle = document.getElementById('rise-title')
